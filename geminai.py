7add9a534357dec2d26e3602d4623b57fc3ac6d1
Vishnu J Narayanan <144352066+Vishnuj-n@users.noreply.github.com>
2024-11-30 08:09:35 +0530

    Update geminai.py

--- geminai.py
+++ geminai.py
@@ -39,10 +39,10 @@ def get_health_recommendations(prediction, sex, age, accuracy):
     <basic information of patient in numbered  points in natural language >
 
     #Recommendations:
-    doctor_consultation:<"if required `yes` else `no`">
+    <doctor_consultation:<"if required `yes` else `no`">
     tests:<if required `yes` else `no`. if `yes` suggestions >
     physical_activity:<excerise/yoga/ recommended for heart patients if target ='1' else some good physical_activity >
-    warning:<this is model prediction which can't replace the need for professional medical evaluation>
+    warning:<this is model prediction which can't replace the need for professional medical evaluation>>
 
     #Reducing Impact (Patient Communication):
     < 5 numbered points on patient communication >
